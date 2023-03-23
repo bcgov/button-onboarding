efd12a2e290a66f284dd772214e6ca874ccfa45b
JoshLarouche <joshua.t.larouche@gmail.com>
2023-03-23 16:33:17 -0700

    Added todo_app schema

--- schema/deploy/appschema.sql
+++
@@ -1,8 +0,0 @@
--- Deploy schema:appschema to pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-CREATE SCHEMA todo;
-
-COMMIT;
--- schema/revert/appschema.sql
+++
@@ -1,8 +0,0 @@
--- Revert schema:appschema from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-DROP SCHEMA todo;
-
-COMMIT;
